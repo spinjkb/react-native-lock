88326f707a9702651ae21dd615c4a07e7bb1f458
靳凯博 <960713393@qq.com>
2018-02-06 17:24:14 +0800

    更新navigationbar

--- src/view/OneRoom.js
+++ src/view/OneRoom.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { View, Text, ScrollView, Image, Dimensions, StyleSheet, ActivityIndicator } from 'react-native';
 
-import { NavigationPage,NavigationBar, Label, Carousel, PullPicker, ListRow, Toast, Theme, Button } from 'teaset';
+import { NavigationPage, NavigationBar, Label, Carousel, PullPicker, ListRow, Toast, Theme, Button } from 'teaset';
 import axios from 'axios'
 import rs from '../mobx/roomStorage'
 export default class OneRoom extends NavigationPage {
@@ -14,12 +14,13 @@ export default class OneRoom extends NavigationPage {
     constructor(props) {
         super(props)
         // this.items = ['none', 'default', 'custom'];
+        // this.leftViewItems = ['None', 'Back button', 'Link button', 'Icon button', 'Two icon button'];
         this.state = {
             width: Dimensions.get('window').width,
             control: 'default',
             roomid: '',
             loading: true,
-            roomdetail:''
+            roomdetail: ''
         }
     }
     componentDidMount() {
@@ -33,7 +34,7 @@ export default class OneRoom extends NavigationPage {
         }).then(function (response) {
             if (response.data.meta.code === 200) {
                 _this.hideCustom()
-                _this.setState({ loading: false, roomid: rs.getSelectRoomId() ,roomdetail:response.data.data.message})
+                _this.setState({ loading: false, roomid: rs.getSelectRoomId(), roomdetail: response.data.data })
                 // rs.setRoom(response.data.data.room)
                 console.log(response.data)
             }
@@ -61,18 +62,28 @@ export default class OneRoom extends NavigationPage {
         // console.log(this.state)
         // console.log(rs.getSelectRoomId())
         // console.log(rs.getSelectRoomTitle())
-        this.props.title=rs.getSelectRoomTitle()
-        
+        this.props.title = rs.getSelectRoomTitle()
+
         console.log(this)
     }
-    renderNavigationBar() {
-        // 重新渲染标题
+   
+    // renderNavigationBar() {
+    //     // 重新渲染顶部。按钮需要重写
+    //     return (
+    //         <NavigationBar
+    //             title={rs.getSelectRoomTitle()}
+    //             showBackButton={true}
+    //         />
+    //     );
+    // }
+    renderNavigationTitle() {
+        // 只重新渲染标题
         return (
-          <NavigationBar
-            title={rs.getSelectRoomTitle()}
-            />
+            <View >
+                <Label style={{ color: Theme.navTitleColor, fontSize: 17 }} text={rs.getSelectRoomTitle()} />
+            </View>
         );
-      }
+    }
     renderPage() {
         // console.log(rs.getSelectRoomId())
         let { width } = this.state;
@@ -86,10 +97,14 @@ export default class OneRoom extends NavigationPage {
                         control={true}
                         onLayout={e => this.setState({ width: e.nativeEvent.layout.width })}
                     >
-                        <Image style={{ width, height: 238 }} resizeMode='cover' source={require('../images/teaset1.jpg')} />
-                        <Image style={{ width, height: 238 }} resizeMode='cover' source={require('../images/teaset2.jpg')} />
-                        <Image style={{ width, height: 238 }} resizeMode='cover' source={require('../images/teaset3.jpg')} />
+                        {this.state.roomdetail.image.map((item, index) => {
+                            return <Image key={index} style={{ width, height: 238 }} resizeMode='cover'
+                                source={{ uri: item }} />
+                        })}
                     </Carousel>
+                    <View style={{ height: 20 }} />
+                    <ListRow title='可容纳人数' detail={this.state.roomdetail.capacity + '人'} topSeparator='full' bottomSeparator='full' />
+                    <ListRow title='价格' detail={this.state.roomdetail.price + '/小时'} topSeparator='full' bottomSeparator='full' />
                     <View style={{ height: 20 }} />
                     <ListRow title='click' detail={this.state.control} onPress={() => this.show()} topSeparator='full' bottomSeparator='full' />
                 </ScrollView>
