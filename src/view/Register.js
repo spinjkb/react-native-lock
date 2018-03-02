1294b0e5d79edddec9a25d3b65cda5a7485a8752
靳凯博 <960713393@qq.com>
2018-03-02 14:06:28 +0800

    手机注册

--- src/view/Register.js
+++ src/view/Register.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import {View} from 'react-native';
-import {Navigator} from 'react-native-deprecated-custom-components'
-import {BasePage, Input, Button,NavigationPage} from 'teaset';
+import { View } from 'react-native';
+import { Navigator } from 'react-native-deprecated-custom-components'
+import { BasePage, Input, Button, NavigationPage } from 'teaset';
 
 import axios from 'axios'
 
@@ -15,51 +15,131 @@ export default class Register extends NavigationPage {
 
   constructor(props) {
     super(props);
-    this.state={
-      userName: null,
+    this.state = {
+      mobile: null,
+      username:'',
       password: null,
-     
+      code: null,
+      transactionid:this.randomString(20),
     }
   }
-  register=()=>{
+  register = () => {
+    if (!this.checkMobile(this.state.mobile)) {
+      alert('请检查手机号')
+      return
+    }
+    if (this.state.username==='') {
+      alert('请输入用户名')
+      return
+    }
     let _this = this
+    var params = new FormData();
+    params.append('mobile', this.state.mobile)
+    params.append('transactionid', this.state.transactionid)
+    params.append('code', this.state.code)
+    params.append('password', this.state.password)
+    params.append('first_name', this.state.username)
 
-    axios.get('http://localhost:3000/register',{
-      params:{
-        userName:this.state.userName,
-        password:this.state.password
-      }
-    }).then(function(response){
-    
-      if(response.data.meta.code ===200){
-        alert(response.data.data.message)
-        _this.setState({userName:'',password:''})
-      }
-      console.log(response.data);
+    var qs = require('qs')
+    var form = {
+      mobile: this.state.mobile,
+      action_type: 'register',
+      transactionid: this.state.transactionid
+    }
+    axios.post('http://47.92.72.19:9000/meetuser/checkAndRegister', qs.stringify(form)).then(function (response) {
+      console.log(response)
+    }).catch(function (error) {
+      console.log(error)
     })
-    .catch(function(err){
-      console.log(err);
-    });
+  }
+  send = () => {
+    if (!this.checkMobile(this.state.mobile)) {
+      alert('请检查手机号')
+      return
+    }
+    let _this = this
+    var params = new FormData();
+    params.append('mobile',this.state.mobile)
+    params.append('action_type','register')
+    params.append('transactionid',this.state.transactionid)
+    var qs = require('qs')
+    var form = {
+      mobile:this.state.mobile,
+      action_type:'register',
+      transactionid:this.state.transactionid
+    }
+    axios.post('http://47.92.72.19:9000/meetuser/send_sms',qs.stringify(form)).then(function(response){
+      console.log(response)
+    }).catch(function(error){
+      console.log(error)
+    })
+  }
+  //生成随机数
+  randomString(len) {
+    len = len || 32;
+    　　var $chars = 'ABCDEFGHJKMNPQRSTWXYZabcdefhijkmnprstwxyz2345678';    /****默认去掉了容易混淆的字符oOLl,9gq,Vv,Uu,I1****/
+    　　var maxPos = $chars.length;
+    　　var pwd = '';
+    　　for (i = 0; i < len; i++) {
+      　　　　pwd += $chars.charAt(Math.floor(Math.random() * maxPos));
+    　　}
+    　　return pwd;
+  }
+  checkMobile(str) {
+    re = /^1\d{10}$/
+    if (re.test(str)) {
+      
+      return true
+    } else {
+      
+      return false
+    }
   }
   renderPage() {
     return (
-      <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
         <Input
-          style={{width: 200}}
+          style={{ width: 200 }}
           size='sm'
-          value={this.state.userName}
-          placeholder='用户名'
-          onChangeText={text => this.setState({userName: text})}
-          />
+          value={this.state.mobile}
+          placeholder='手机号'
+          onChangeText={text => this.setState({ mobile: text })}
+        />
         <Input
-          style={{width: 200}}
+          style={{ width: 200 }}
+          size='sm'
+          value={this.state.username}
+          autoCapitalize="none"
+          autoCorrect='false'
+          placeholder='用户名'
+          onChangeText={text => this.setState({ username: text })}
+        />
+        <Input
+          style={{ width: 200 }}
           size='sm'
           value={this.state.password}
           placeholder='密码'
+          autoCapitalize="none"
+          autoCorrect='false'
           secureTextEntry={true}
-          onChangeText={text => this.setState({password: text})}
-          />
-        <Button title='注册' onPress={this.register}/>
+          onChangeText={text => this.setState({ password: text })}
+        />
+        <Input
+          style={{ width: 200 }}
+          size='sm'
+          value={this.state.code}
+          placeholder='验证码'
+          secureTextEntry={true}
+          onChangeText={text => this.setState({ code: text })}
+        />
+        <Input
+          style={{ width: 200,display: 'none', }}
+          size='sm'
+          value={this.state.transactionid}
+
+        />
+        <Button title='发送验证码' onPress={this.send}/>
+        <Button title='注册' onPress={this.register} />
       </View>
     );
   }
